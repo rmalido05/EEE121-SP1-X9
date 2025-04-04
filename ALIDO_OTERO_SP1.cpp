16684806972aafb471c525a0644146e0b2884861
rmalido05 <alidorenzo1546@gmail.com>
2025-04-05 00:05:58 +0800

    Update ALIDO_OTERO_SP1.cpp
    I. Rearrangements
    - Lines 13-19
    - Lines 32-34
    
    II. Added comments
    - "For debugging purposes". (ex. Line 55)
    
    III. menuInterface()
    - Changed design of items. (from 1 - Prompt -> [1] Prompt)
    
    IV. addInterface()
    - Used id like a counter.
    - Every iteration of the while loop, id++.
    - CLI prompts user to enter nth choice
    - Added cin.ignore() to resolve problem w.r.t skipping choice A
    - Resets variable values (clears data from question, answer, and sets difficulty to 0 before next iteration)
    
    V. studyInterface()
    - Changed from void function to bool function.
    - Program now counts correct answers and tells whether or not the user's answer is correct.
    - Defined variables question, flash_answer, and choices using flashcard data.
    - Added counter.
    - Created new variable user_answer to address user input.
    
    VI. flashcardInterfare()
    - Changed item design. (A. -> [A])

--- ALIDO_OTERO_SP1.cpp
+++ ALIDO_OTERO_SP1.cpp
@@ -1,162 +1,256 @@
 #include <iostream>
-
+#include <vector>
 #include <string>
+#include <tuple>
+#include <queue>
 
 using namespace std;
 
-template<typename S>
-class BinaryNode {
+class Flashcard {
     public:
-        S diff;
-        BinaryNode* right;
-        BinaryNode* left;
-    
-        BinaryNode(S val);
-    //template<typename> friend class FlashcardBST;
+        int id, difficulty;
+        string question, answer;
+        vector<string> choices;
+        Flashcard(int id, int difficulty, string question, string answer, vector<string> choices) 
+            : id(id),
+              difficulty(difficulty),
+              question(question),
+              answer(answer),
+              choices(choices) 
+        {}
 };
 
-template<typename S>
-BinaryNode<S>::BinaryNode(S diff):right(NULL),left(NULL),diff(diff) {}
-
-template<typename Card>
 class FlashcardBST {
-    private: 
-        BinaryNode<Card>* root;
-        int size;
-        void addHelper(BinaryNode<Card>* curr, Card diff, string ques, string ans, bool correctness);
-        void deleteHelper(BinaryNode<Card>* curr);
-        void postorderHelper(BinaryNode<Card>* curr);
-        void preorderHelper(BinaryNode<Card>* curr);
-        void inorderHelper(BinaryNode<Card>* curr);
+    private:
+        queue<Flashcard> flashcards;
+    protected:
+        struct Node {
+            tuple<int, queue<Flashcard>> data;
+            Node* right;
+            Node* left;
+
+            Node(int difficulty, queue<Flashcard> flashcards)
+                : data({difficulty, flashcards}), 
+                  right(NULL), 
+                  left(NULL) 
+            {}
+        };
+        Node* root;
+
+        Node* newDiffHelper(Node* currentNode, int difficulty) {
+            if(currentNode == nullptr) {
+                return new Node(difficulty, flashcards);
+            } else if(difficulty < get<0>(currentNode -> data)) {
+                currentNode -> left = newDiffHelper(currentNode -> left, difficulty);
+            } else {
+                currentNode -> right = newDiffHelper(currentNode -> right, difficulty);
+            }
+            return currentNode;
+        }
+
+        Node* findDiffHelper(Node* currentNode, int difficulty) {
+            if (currentNode == nullptr) {
+                return nullptr;
+            }
+            if(difficulty == get<0>(currentNode -> data)) {
+                cout << 0 << endl;          // For debugging purposes
+                return currentNode;
+            } else if(difficulty < get<0>(currentNode -> data)) {
+                cout << 1 << endl;          // For debugging purposes
+                return findDiffHelper(currentNode -> left, difficulty);
+            } else {
+                cout << 2 << endl;          // For debugging purposes
+                return findDiffHelper(currentNode -> right, difficulty);
+            }
+        }
     public:
-        FlashcardBST();
-        void addCard(Card diff, string ques, string ans, bool correctness);
-        void deleteTree();
-        void postorder();
-        void preorder();
-        void inorder();
+        FlashcardBST():root(nullptr) {}
+
+        void newDiff(int difficulty) {
+            root = newDiffHelper(root, difficulty);
+        }
+
+        Node* findDiff(int difficulty) {
+            cout << 0 << endl;                              // For debugging purposes
+            Node* node = findDiffHelper(root, difficulty);
+            return node;
+        }
 };
 
-template<typename Card>
-FlashcardBST<Card>::FlashcardBST():root(NULL),size(0) {}
+class FlashcardSystem : public FlashcardBST {
+    private:
+        void addFlashcardHelper(int id, int difficulty, string question, string answer, vector<string> choices) {
+            cout << 0 << endl;
+            Flashcard flashcard(id, difficulty, question, answer, choices);
+            Node* node = findDiff(difficulty);
 
-template<typename Card>
-void FlashcardBST<Card>::addCard(Card diff, string ques, string ans, bool correctness) {
-    if(this -> root) {
-        this -> addHelper(this -> root, diff, ques, ans, correctness);
-    } else {
-        this -> root  = new BinaryNode<Card>(diff);
-    }
-    (this -> size)++;
-}
+            if (node == nullptr) {
+                newDiff(difficulty);
+                node = findDiff(difficulty);
+            }
 
-template<typename Card>
-void FlashcardBST<Card>::addHelper(BinaryNode<Card>* curr, Card diff, string ques, string ans, bool correctness) {
-    if(curr -> diff >= diff) {
-       if(curr -> left) {
-           this -> addHelper(curr -> left,diff, ques, ans, correctness);
-       } else {
-           curr -> left = new BinaryNode<Card>(diff, ques, ans, correctness);
-       }
-    } else { 
-        if(curr -> right) {
-           this -> addHelper(curr -> right, diff, ques, ans, correctness);
-       } else {
-           curr -> right = new BinaryNode<Card>(diff, ques, ans, correctness);
-       }
-    }
-}
+            get<1>(node->data).push(flashcard);
+            return;
+        }
+        queue<Flashcard> studySystem(int difficulty) {
+            queue<Flashcard> flashcards = get<1>(findDiff(difficulty) -> data);
+            return flashcards;
+        }
+    public:
+        void menuInterface() {
+            bool run = true;
+            int input;
+            while(run) {
+                cout << "====================================================\n";
+                cout << "[MAIN MENU]\n";
+                cout << endl;
+                cout << "[0] Add Flashcard\n";
+                cout << "[1] Edit Flashcard\n";
+                cout << "[2] Delete Flashcard\n";
+                cout << "[3] Study Mode\n";
+                cout << "[4] Exit\n";
+                cout << endl;
+                cout << "Selection: ";
+                cin >> input;
+                cout << "====================================================\n";
 
+                switch(input) {
+                    case 0:
+                        run = addInterface();
+                        break;
+                    case 1:
+                        // editInterface();
+                        break;
+                    case 2:
+                        // deleteInterface();
+                        break;
+                    case 3:
+                        studyInterface();
+                        break;
+                    case 4:
+                        cout << "Thank you!" << endl;
+                        run = false;
+                        break;
+                }
+            }
+        }
 
-template<typename Card>
-void FlashcardBST<Card>::deleteTree() {
-    if(this -> root == NULL) {
-        return;
-    } else {
-        this -> deleteHelper(this -> root);
-    }
-    this -> size = 0;
-}
+        bool addInterface() {
+            bool run = true;
+            int id, difficulty;
+            string question, answer, choice, yn;
+            vector<string> choices;
 
-template<typename Card>
-void FlashcardBST<Card>::deleteHelper(BinaryNode<Card>* curr) {
-    if(curr == NULL) {
-        return;
-    } else {
-        this -> deleteHelper(curr -> left);
-        this -> deleteHelper(curr -> right);
-        delete curr;
-    }
-}
+            id = 0;
+            while(run) {
+                cout << "====================================================\n";
+                cout << "[ADD FLASHCARD]\n";
+                cout << "Enter question: ";
+                getline(cin, question);
+                cin.ignore(); // Clear the newline character from the input buffer
+                for(int j = 0; j < 4; j++) {
+                    cout << "Enter choice" << j + 1 << ": ";
+                    getline(cin, choice);
+                    choices.push_back(choice);
+                    cin.ignore();   
+                    cout << endl;
+                }
 
-template<typename Card>
-void FlashcardBST<Card>::postorder() {
-    if(this -> root == NULL) {
-        cout << "Nothing to print" << endl;
-    } else {
-        postorderHelper(this -> root);
-        cout << endl;
-    }
-}
+                cout << "Enter correct answer: ";
+                getline(cin, answer);
+                cin.ignore();
+                cout << endl;
+                cout << "Difficulty: ";
+                cin >> difficulty;
+                cout << endl;
 
-template<typename Card>
-void FlashcardBST<Card>::postorderHelper(BinaryNode<Card>* curr) {
-    if(curr == NULL) {
-        return;
-    } else {
-        this -> postorderHelper(curr -> left);
-        this -> postorderHelper(curr -> right);
-        cout << "DIFFICULTY: " << curr -> diff << endl;     // Prints the diff value of the card
-        cout << curr -> ques << " ";                        // Prints the question of the card
-    }
-}
+                addFlashcardHelper(id, difficulty, question, answer, choices);
 
+                cout << "Do you want to add another flashcard [Y/N]? ";
+                cin >> yn;
+                cout << "====================================================\n";
+                if(yn == "N" || yn == "n") {
+                    run = false;
+                }
 
-template<typename Card>
-void FlashcardBST<Card>::preorder() {
-    if(this -> root == NULL) {
-        cout << "Nothing to print" << endl;
-    } else {
-        preorderHelper(this -> root);
-        cout << endl;
-    }
-}
+                id++;
+                difficulty = 0; // Reset difficulty for the next flashcard
+                question.clear(); // Clear the question string for the next flashcard
+                answer.clear(); // Clear the answer string for the next flashcard
+                choices.clear(); // Clear the choices vector for the next flashcard
+            }
+            return true;
+        }
 
-template<typename Card>
-void FlashcardBST<Card>::preorderHelper(BinaryNode<Card>* curr) {
-    if(curr == NULL) {
-        return;
-    } else {
-        cout << "DIFFICULTY: " << curr -> diff << endl;
-        cout << curr -> ques << " ";
-        this -> preorderHelper(curr -> left);
-        this -> preorderHelper(curr -> right);
-    }
-}
+        // bool deleteInterface() {
+        //     // delete flashcard
+        //     cout << "====================================================\n";
+        //     cout << "[DELETE FLASHCARD]\n";
+        //     cout << "Choose a flashcard to delete: ";
+        //     int size = flashcards.size();
+        //     for(int i = 1; i < size; i++) {
+        //         cout << "[" << i << "]" << " " << flashcards.question << endl;
+        //     }
+        //     return true;
+        // }
 
-template<typename Card>
-void FlashcardBST<Card>::inorder() {
-    if(this -> root == NULL) {
-        cout << "Nothing to print" << endl;
-    } else {
-        inorderHelper(this -> root);
-        cout << endl;
-    }
-}
+        bool studyInterface() {
+            int difficulty;
+            queue<Flashcard> flashcards;
+            cout << "====================================================\n";
+            cout << "[STUDY MODE]\n";
+            cout << "Select a difficulty: ";
+            cin >> difficulty;
 
-template<typename Card>
-void FlashcardBST<Card>::inorderHelper(BinaryNode<Card>* curr) {
-    if(curr == NULL) {
-        return;
-    } else {
-        this -> inorderHelper(curr -> left);
-        cout << "DIFFICULTY: " << curr -> diff << endl;
-        cout << curr -> ques << " ";
-        this -> inorderHelper(curr -> right);
-    }
-}
+            int correct = 0;
+            flashcards = studySystem(difficulty);
+            int size = flashcards.size();
+            
+            for(int i = 0; i <= size; i++) {
+                string question = flashcards.front().question;
+                string flash_answer = flashcards.front().answer;
+                vector<string> choices = flashcards.front().choices;
+                int ctr = i + 1;
+                string user_answer;
+
+                flashcardInterface(question, flash_answer, choices, ctr);                
+                cout << "Enter your answer: ";  
+                
+                getline(cin, user_answer);
+                cin.ignore();
+                cout << endl << endl;
+                if (user_answer == flash_answer) {
+                    correct++; 
+                    cout << "Your answer is correct!" << endl;
+                } else {
+                    cout << "Incorrect! The correct answer is: " << flashcards.front().answer << endl;
+                }
+                flashcards.pop();
+                cout << endl;
+            }
+            cout << endl << "==================================================== \n";
+            cout << "You have answered " << correct << " out of " << size << " questions correctly. \n";
+            cout << endl;
+            return true;
+        }
+
+        void flashcardInterface(string question, string answer, vector<string> choices, int ctr) {
+            cout << "====================================================\n";
+            cout << "Question #" << ctr << ": " << question << endl;
+            cout << "Choices: " << endl;
+            cout << "[A] " << choices[0] << endl;
+            cout << "[B] " << choices[1] << endl;
+            cout << "[C] " << choices[2] << endl;
+            cout << "[D] " << choices[3] << endl;
+            return;
+        }
+};
 
 main(){
-    // Place main code here.
+    FlashcardSystem flash;
+    FlashcardBST ez;
+    ez.newDiff(1);
+    ez.newDiff(3);
+    flash.menuInterface();
     return 0;
 }
